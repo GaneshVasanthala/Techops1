3111327b7ffe9bca42aea7c9e8644b36507b2aa6
GaneshVasanthala <vasanthservice1729@gmail.com>
2025-07-25 20:46:31 +0530

    Update bfomadam.py

--- bfomadam.py
+++ bfomadam.py
@@ -2,7 +2,7 @@
     Goiod afternoon this is the new session on bfo topic discussed by keerti madam 
 
 
-
+Hello new ionformatioon has been done over here to understand what needs tio be done 
 
 Hence there fore this is yet to undertsnad abiout 
 this ijust to ionform to the new one to increase the data 
