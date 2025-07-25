6bcf545e65fb291160211e2603e53e5279659b92
GaneshVasanthala <vasanthservice1729@gmail.com>
2025-07-25 20:44:31 +0530

    new changes has been added

--- bfomadam.py
+++ bfomadam.py
@@ -1,4 +1,8 @@
     Hello 
     Goiod afternoon this is the new session on bfo topic discussed by keerti madam 
 
+
+
+
+Hence there fore this is yet to undertsnad abiout 
 this ijust to ionform to the new one to increase the data 
